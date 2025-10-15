8fc7d0b9586955fdc4cfcb8bf9be7df6c0cf0308
Andrei Ignat <ignatandrei@yahoo.com>
2025-10-15 22:48:25 +0300

    modified aspire to run without https

--- src/GenerateTest/tests/example.spec.ts
+++ src/GenerateTest/tests/example.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('https://localhost:17197/login?t=2dbbbbebe0c5aa573090d43b92a59566');
+  await page.goto('http://localhost:15102/');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/SqlServerExtensions resources/);
