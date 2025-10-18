fafa497665d38b6b8a33965ba4b083535e9b1a9e
Andrei Ignat <ignatandrei@yahoo.com>
2025-10-18 16:28:06 +0300

    adding video

--- src/GenerateTest/tests/example.spec.ts
+++ src/GenerateTest/tests/example.spec.ts
@@ -7,3 +7,9 @@ test('has title', async ({ page }) => {
   await expect(page).toHaveTitle(/SqlServerExtensions resources/);
 });
 
+test.afterEach(async ( {page}) => {
+      // Take a screenshot after each test
+      await page.screenshot({ path: `screenshot-${Date.now()}.png` });
+      if(page.video() !== null  )
+        await page.video()!.saveAs(`video-${Date.now()}.webm`);
+  });
--- src/GenerateTest/tests/seed.spec.ts
+++ src/GenerateTest/tests/seed.spec.ts
@@ -1,7 +1,14 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, Page } from '@playwright/test';
 
 test.describe('Test group', () => {
   test('seed', async ({ page }) => {
     // generate code here.
   });
+   test.afterEach(async ( {page}) => {
+      // Take a screenshot after each test
+      await page.screenshot({ path: `screenshot-${Date.now()}.png` });
+      if(page.video() !== null  )
+        await page.video()!.saveAs(`video-${Date.now()}.webm`);
+  });
+
 });
