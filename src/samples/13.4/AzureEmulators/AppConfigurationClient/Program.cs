6716db71283b947f7f896b8c100699b63a07d9fc
Andrei Ignat <ignatandrei@yahoo.com>
2026-06-24 20:54:05 +0300

    app config

--- src/samples/13.4/AzureEmulators/AppConfigurationClient/Program.cs
+++ src/samples/13.4/AzureEmulators/AppConfigurationClient/Program.cs
@@ -1,4 +1,6 @@
-﻿using Azure.Data.AppConfiguration;
+﻿using Azure.Core;
+using Azure.Core.Pipeline;
+using Azure.Data.AppConfiguration;
 using System.Collections;
 
 Console.WriteLine("Hello,AzureAppConfiguration!");
@@ -18,9 +20,29 @@ if (string.IsNullOrWhiteSpace(connectionStringAzureAppConfig))
     Console.WriteLine("Azure App Configuration connection string is not set in environment variables.");
     return;
 }
+
+ConfigurationClientOptions clientOptions = new ConfigurationClientOptions();
+clientOptions.AddPolicy(new RemoveAuthorizationHeaderPolicy(), HttpPipelinePosition.PerRetry);
+
 Console.WriteLine($"Connection string for Azure App Configuration: {connectionStringAzureAppConfig}");
-var client = new ConfigurationClient(connectionStringAzureAppConfig);
+var client = new ConfigurationClient(connectionStringAzureAppConfig, clientOptions);
 await client.SetConfigurationSettingAsync("Name","Andrei");
 Console.WriteLine("Set configuration setting 'Name' to 'Andrei' in Azure App Configuration.");
 var name = await client.GetConfigurationSettingAsync("Name");
 Console.WriteLine($"Retrieved configuration setting 'Name' from Azure App Configuration: {name.Value}");
+
+
+internal sealed class RemoveAuthorizationHeaderPolicy : HttpPipelinePolicy
+{
+    public override void Process(HttpMessage message, ReadOnlyMemory<HttpPipelinePolicy> pipeline)
+    {
+        message.Request.Headers.Remove("Authorization");
+        ProcessNext(message, pipeline);
+    }
+
+    public override ValueTask ProcessAsync(HttpMessage message, ReadOnlyMemory<HttpPipelinePolicy> pipeline)
+    {
+        message.Request.Headers.Remove("Authorization");
+        return ProcessNextAsync(message, pipeline);
+    }
+}
