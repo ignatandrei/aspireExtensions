2db43c336e62f23b64b2512f1bc172f992d9db01
Andrei Ignat <ignatandrei@yahoo.com>
2026-08-16 22:18:29 +0300

    suggestions

--- src/AspireFileDisplayExtension/AspireFileDisplayExtension/Templates/DisplayAllFiles.cs
+++ src/AspireFileDisplayExtension/AspireFileDisplayExtension/Templates/DisplayAllFiles.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace AspireFileDisplayExtension.Templates;
+﻿namespace AspireFileDisplayExtension.Templates;
 
 internal class DisplayAllFiles
 {
